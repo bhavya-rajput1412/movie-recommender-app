f497aa21b334bc52c19aee216ee8638fdd53f449
bhavya-rajput1412 <bhavya.rajput1412@gmail.com>
2026-05-05 16:56:33 +0530

    fixed similarity.pkl gzip loading

--- movie_app.py
+++ movie_app.py
@@ -158,7 +158,8 @@ div[data-testid="stButton"] > button:hover {
 def load_data():
     movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
     movies = pd.DataFrame(movies_dict)
-    similarity = pickle.load(open('similarity.pkl', 'rb'))
+    with gzip.open('similarity.pkl', 'rb') as f:
+        similarity = pickle.load(f)
     return movies, similarity
 
 movies, similarity = load_data()
